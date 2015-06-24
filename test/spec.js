6eff0d3063c7c9fbf25a3cd62ce02344cde9f85c
Anthony Barnes <abarnes@squiz.com.au>
2015-06-24 10:09:28 +1000

    Modified the stream to use through2 for consistency. Added unit tests

--- test/spec.js
+++ test/spec.js
@@ -1,20 +1,20 @@
-/*global describe, before, it */
+/*global describe, it */
 'use strict';
-var fs = require('fs'),
-  path = require('path'),
-  chai = require('chai'),
-  expect = chai.expect,
-  t = require('../index.js');
+var fs = require('fs');
+var path = require('path');
+var chai = require('chai');
+var expect = chai.expect;
+var t = require('../index.js');
+var through = require('through2');
 
 chai.should();
 
 var files = {
-  basic: path.resolve(__dirname, './fixtures/basic.html')
+  basic: path.resolve(__dirname, './fixtures/basic.html'),
+  index: path.resolve(__dirname, './fixtures/index.html')
 };
 
 describe('Tokeniser', function() {
-
-  this.timeout(10*1000);
 
   it('can parse basic html into tokens', function (done) {
     var tokens = [];
@@ -33,17 +33,50 @@ describe('Tokeniser', function() {
       });
   });
 
+  it('can parse a variety of HTML into tokens', function (done) {
+    var tokens = [];
+    fs.createReadStream(files.index)
+      .pipe(t.tokeniser())
+      .on('data', function(token) {
+        tokens.push(token);
+      })
+      .on('finish', function() {
+        expect(tokens[0].type).to.equal('processinginstruction');
+        expect(tokens[0].content).to.equal('!DOCTYPE html');
+        var lastToken = tokens.pop();
+        expect(lastToken.type).to.equal('close');
+        expect(lastToken.name).to.equal('html');
+        done();
+      });
+  });
+
+  it('can be piped to through2 in object mode', function (done) {
+    var tokens = [];
+    fs.createReadStream(files.index)
+      .pipe(t.tokeniser())
+      .pipe(through.obj(function(token, enc, cb) {
+        tokens.push(token);
+        this.push(token);
+        cb();
+      }))
+      // Without resuming this stream remains frozen in time
+      // @todo - investigate
+      .resume()
+      .on('finish', function() {
+        expect(tokens.length).to.equal(71);
+        done();
+      });
+  });
+
   it('can take tokens and turn them back into HTML', function (done) {
     var content = fs.readFileSync(files.basic, {encoding: 'utf8'});
     var html = '';
     fs.createReadStream(files.basic)
       .pipe(t.tokeniser())
-      .pause()
       .pipe(t.toHTML())
       .on('data', function(data) {
         html += data;
       })
-      .resume()
       .on('finish', function() {
         // Resulting HTML should be the same as the original content
         expect(content).to.equal(html);
@@ -53,7 +86,7 @@ describe('Tokeniser', function() {
 
   it('can convert tokens ad hoc by exposing a conversion function', function (done) {
     var token = {
-      type: 'comment',
+      type:    'comment',
       content: ' This is a comment '
     };
     expect(t.toHTML.convertTokenToHTML(token)).to.equal('<!-- This is a comment -->');
